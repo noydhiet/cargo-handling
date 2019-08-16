5456f15dacda1d4d83b1720fa248eb88e8fce12f
adlymaulana <adly.ms@gmail.com>
2019-08-16 17:35:40 +0700

    Pembaharua

--- service/service.go
+++ service/service.go
@@ -1,24 +1,18 @@
 package service
 
 import (
-	// "fmt"
-	// "strings"
 	"database/sql"
-	// "log"
-	// "net/http"
-	// "text/template"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Connect to database
 func dbConn() (db *sql.DB) {
 	dbDriver := "mysql"
 	dbUser := "root"
 	dbPass := "PASSWORD"
 	dbName := "db_go"
-	dbIP := "192.168.20.9"
-
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbIP+")/"+dbName)
+	dbIp := "192.168.20.9"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbIp+")/"+dbName)
 
 	if err != nil {
 		panic(err.Error())
@@ -27,4 +21,25 @@ func dbConn() (db *sql.DB) {
 	return db
 }
 
+func viewALl(){
 
+	db := dbConn()
+	selDb, err := db.Query("SELECT ROUTING_STATUS, TRANS_STATUS FROM t_trx_delivery WHERE ROUTE_ID = ITENARY_ID")
+
+	if err != nil{
+		panic(err.Error())
+	}
+}
+
+
+func Update(w http.ResponseWriter, r *http.Request){
+	db := dbConn()
+
+	updForm, err := db.Prepare("UPDATE t_trx_delivery SET ROUTING_STATUS = success, TRANSPORT_STATUS = success ")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+}
--- transport/transport.go
+++ transport/transport.go
@@ -1,2 +1,76 @@
 package transport
 
+import (
+	"cargo-Handling/datastruct"
+	dt "cargo-Handling/datastruct"
+	svc "cargo-Handling/service"
+	"context"
+	"encoding/json"
+	"log"
+
+	//"kit/endpoint"
+	"net/http"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type AphService interface {
+	UpdateStatusHandlingService(context.Context, dt.Handling) []dt.Handling
+}
+
+type aphService struct{
+
+}
+
+func (aphService) UpdateStatusHandlingService(_ context.Context, del dt.Handling) []dt.Handling {
+	return call_UpdateStatusHandlingService(hand)
+}
+
+func call_ServiceUpdateStatusHandlingService(hand dt.Handling) []dt.Handling {
+	retHand := svc.HandStatusHandling(hand)
+
+	return retHand
+}
+
+func makeUpdateStatusHandlingEndpoint(aph AphService) endpoint.Endpoint {
+	log.Println("Process")
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(datastruct.UpdateStatusHandlingRequest)
+		paramHand := dt.Handling{}
+		paramHand.ID_ROUTE = req.ROUTE_ID
+		paramHand.ID_ITENARY = req.ITENARY_ID
+		paramHand.STATUS_outing_status = req.ROUTING_STATUS
+		paramHand.STATUS_TRANSPORT = req.TRANSPORT_STATUS
+		aph.UpdateStatusHandlingService(ctx, paramHand)
+		return datastruct.GetStatusDeliveryResponse{
+			ROUTING_STATUS:      "Success",
+			TRANSPORT_STATUS:    "Success",
+		}, nil
+	}
+}
+
+func decodeUpdateHandlingStatusRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request datastruct.GetStatusDeliveryRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func RegisterHttpsServicesAndStartListener() {
+	aph := aphService{}
+
+	GetStatusDeliveryHandler := httptransport.NewServer(
+		makeGetStatusDeliveryEndpoint(aph),
+		decodeGetDeliveryStatusRequest,
+		encodeResponse,
+	)
+	//url path of our API service
+	http.Handle("/GetStatusDelivery", GetStatusDeliveryHandler)
+
+}
